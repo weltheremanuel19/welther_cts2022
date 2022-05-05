d3b028e9d0872012fa8b173a9e697258debbf06f
weltheremanuel19 <91604495+weltheremanuel19@users.noreply.github.com>
2022-05-05 15:00:03 +0300

    .

---
+++ seminar1/src/ro/ase/dice/clase/IngrijitorZOO.java
@@ -0,0 +1,21 @@
+package ro.ase.dice.clase;
+
+public class IngrijitorZOO {
+    private String nume;
+
+    public IngrijitorZOO(String nume) {
+        this.nume = nume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public void hranesteAnimal(Animal animal, String mancare){
+        animal.manancaAnimal(mancare);
+    }
+}
