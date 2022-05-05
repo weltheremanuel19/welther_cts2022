25c2629df2105a027919332525e6a8b75a99d1b0
weltheremanuel19 <91604495+weltheremanuel19@users.noreply.github.com>
2022-05-05 15:23:59 +0300

    .
    # Conflicts:
    #	src.ro.ase.dice/clase/Animal.java
    #	src.ro.ase.dice/clase/Elefant.java
    #	src.ro.ase.dice/clase/IngreijitorZOO.java
    #	src.ro.ase.dice/clase/IngrijitorZOO.java
    #	src.ro.ase.dice/clase/ZOO.java
    #	src.ro.ase.dice/clase/Zebra.java
    #	src.ro.ase.dice/main/Main.java

---
+++ zoo/src/ro/ase/dice/clase/Elefant.java
@@ -0,0 +1,18 @@
+package ro.ase.dice.clase;
+
+public class Elefant extends Animal{
+    public Elefant(String nume) {
+        super(nume);
+    }
+
+    @Override
+    public void manancaAnimal(String mancare) {
+        System.out.println("Elefantul" + this.getNume() + "mananca" + mancare);
+
+    }
+
+    @Override
+    public void mananca(String mancare) {
+
+    }
+}
