ca87c7365e288e7a1137b4daf165440029674f73
weltheremanuel19 <91604495+weltheremanuel19@users.noreply.github.com>
2022-05-29 11:36:05 +0300

    .
    # Conflicts:
    #	src.ro.ase.dice/clase/Animal.java
    #	src.ro.ase.dice/clase/Elefant.java
    #	src.ro.ase.dice/clase/IngreijitorZOO.java
    #	src.ro.ase.dice/clase/IngrijitorZOO.java
    #	src.ro.ase.dice/clase/ZOO.java
    #	src.ro.ase.dice/clase/Zebra.java
    #	src.ro.ase.dice/main/Main.java

--- seminar7_Observer_1/src/ro/ase/dice/clase/Calator.java
+++ seminar7_Observer_1/src/ro/ase/dice/clase/Calator.java
@@ -1,9 +1,14 @@
 package ro.ase.dice.clase;
 
-public record Calator(String nume) implements ICalator {
+public class Calator implements ICalator{
+    private String nume;
+
+    public Calator(String nume) {
+        this.nume = nume;
+    }
 
     @Override
     public void primireMesaj(String mesaj) {
-        System.out.println(this.nume + " ai primit mesajul: " + mesaj);
+        System.out.println(this.nume + " ai primit mesajul: "+ mesaj);
     }
 }
